caf330c2d002a7897607ca3d7c712fc1c68b2faa
amantesf <tesfayee.amanuel@gmail.com>
2026-03-26 17:43:13 -0400

    Removing duplicates from list

--- practice/exercise_11.py
+++ practice/exercise_11.py
@@ -7,3 +7,5 @@ Given Input: data = [1, 2, 2, 3, 4, 4, 4, 5]
 Expected Output: Unique List: [1, 2, 3, 4, 5]
 '''
 data = [1, 2, 2, 3, 4, 4, 4, 5]
+unique_list = list(set(data))
+print(f'Unique List: {data}')
