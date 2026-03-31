77244706473010b27f1cfe33687439fec2e22c73
amantesf <tesfayee.amanuel@gmail.com>
2026-03-31 18:05:38 -0400

    Simple array checking with for loop

---
+++ practice/coding_bat/Warmup_2/array_count9.py
@@ -0,0 +1,20 @@
+'''Given an array of ints, return the number of 9's in the array.
+
+
+array_count9([1, 2, 9]) → 1
+array_count9([1, 9, 9]) → 2
+array_count9([1, 9, 9, 3, 9]) → 3'''
+
+def array_count9(nums):
+  count = 0
+  for a in nums:
+    if a == 9:
+      count += 1
+    else:
+      continue
+  print(count)
+  return count
+
+array_count9([1, 2, 9])
+array_count9([1, 9, 9])
+array_count9([1, 9, 9, 3, 9])
