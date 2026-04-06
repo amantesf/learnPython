574e7a483cf90785cfdbb0859c1a7513444d6a65
amantesf <tesfayee.amanuel@gmail.com>
2026-04-06 15:21:17 -0400

    Added reduce

--- lessons/functions/func_program.py
+++ lessons/functions/func_program.py
@@ -40,6 +40,6 @@ from functools import reduce
 
 nums = [1, 2, 3, 4, 5]
 
-print(reduce(lambda x, y: x + y, nums))
+print(reduce(lambda x, y: x + y, nums)) 
 print(reduce(lambda x, y: x * y, nums))
  
