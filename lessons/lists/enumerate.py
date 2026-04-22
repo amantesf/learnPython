595cfe0ce9142fc4e9a21ed09c086ccced768a47
amantesf <tesfayee.amanuel@gmail.com>
2026-04-22 19:52:39 -0400

    Enumerating for loop to get index

---
+++ lessons/lists/enumerate.py
@@ -0,0 +1,8 @@
+for index, item in enumerate([20, 30, 40]):
+    print(index, item)
+
+countries = ['US', 'China', 'Russia', 'UK', 'Japan', 'Germany']
+for index, i in enumerate(countries):
+    if i == 'US':
+        print(f'The country {i} has been found at index {index}')
+
